207464cb21c1a580d57a7b86d7ec0d5a61a2d65f
shjang7 <lucy.sh.jang@gmail.com>
2019-06-13 00:58:57 +0900

    Add challenge

---
+++ grains/grains.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Grains
+  class << self
+    attr_reader :group, :total
+
+    def initialize
+      @@group = nil
+      @@total = nil
+    end
+
+    def square(n)
+      raise ArgumentError if out_range(n)
+
+      set_grains[n - 1]
+    end
+
+    def total
+      total ||= set_total
+    end
+
+    private
+
+    def set_grains
+      group ||= (1..64).map { |e| 2**(e - 1) }
+    end
+
+    def set_total
+      set_grains.inject(0) { |acc, e| acc += e }
+    end
+
+    def out_range(n)
+      (n < 1) || (n > 64)
+    end
+  end
+end
