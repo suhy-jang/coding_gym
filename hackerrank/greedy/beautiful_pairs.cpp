796928d119032d5e9a286d480f59885908739a4d
Suhyeon <lucy.sh.jang@gmail.com>
2019-07-29 12:21:08 +0900

    Update beautiful_pairs.cpp

--- hackerrank/greedy/beautiful_pairs.cpp
+++ hackerrank/greedy/beautiful_pairs.cpp
@@ -1,23 +1,20 @@
 /* https://www.hackerrank.com/challenges/beautiful-pairs/problem */
 
 int beautifulPairs(vector<int> A, vector<int> B) {
-    map<int, int> m1, m2;
-    int pair;
+    map<int, int> m;
+    int pair = 0;
     bool flag = false;
 
     for(int& num : A) {
-        m1[num]++;
+        m[num]++;
     }
     for(int& num : B) {
-        m2[num]++;
-    }
-    map<int, int>::iterator itr = m1.begin();
-    while(itr != m1.end()) {
-        pair += min(itr->second, m2[itr->first]);
-        if(itr->second != m2[itr->first]) {
+        m[num]--;
+        if(m[num] >= 0) {
+            pair++;
+        } else {
             flag = true;
         }
-        itr++;
     }
     if(flag) return pair + 1;
     return pair - 1;
