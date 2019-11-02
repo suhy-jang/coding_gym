50b3b8f60aed1f772d61a4e9298eaebc90d769d8
Suhyeon Jang <lucy.sh.jang@gmail.com>
2019-11-02 18:37:41 +0900

    Create battleships-in-a-board.rb

---
+++ leetcode/battleships-in-a-board.rb
@@ -0,0 +1,21 @@
+# @param {Character[][]} board
+# @return {Integer}
+def battleship_start_count(board, row, col)
+    return 0 if row-1 >= 0 && board[row-1][col] == 'X'
+    return 0 if col-1 >= 0 && board[row][col-1] == 'X'
+    return 0 unless board[row][col] == 'X'
+    count = 0
+    count += 1 if board[row][col] == 'X'
+    count += 1 if (row+1 < board.size && board[row+1][col] === 'X') && (col+1 < board[0].size && board[row][col+1] == 'X')
+    count
+end
+
+def count_battleships(board)
+    count = 0
+    0.upto(board.size-1) do |r|
+        0.upto(board[0].size-1) do |c|
+            count += battleship_start_count(board, r, c)
+        end
+    end
+    count
+end
