16a53f54e692daccab9b1aafd83d4aaf992d8360
Suhyeon Jang <lucy.sh.jang@gmail.com>
2019-11-02 18:40:08 +0900

    Update battleships-in-a-board.rb

--- leetcode/battleships-in-a-board.rb
+++ leetcode/battleships-in-a-board.rb
@@ -3,10 +3,13 @@
 def battleship_start_count(board, row, col)
     return 0 if row-1 >= 0 && board[row-1][col] == 'X'
     return 0 if col-1 >= 0 && board[row][col-1] == 'X'
-    return 0 unless board[row][col] == 'X'
     count = 0
-    count += 1 if board[row][col] == 'X'
-    count += 1 if (row+1 < board.size && board[row+1][col] === 'X') && (col+1 < board[0].size && board[row][col+1] == 'X')
+    if board[row][col] == 'X'
+        count += 1 
+        if (row+1 < board.size && board[row+1][col] === 'X') && (col+1 < board[0].size && board[row][col+1] == 'X')
+            count += 1 
+        end
+    end
     count
 end
 
