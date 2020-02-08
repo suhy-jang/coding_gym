ff44435ea781289ffc6592bf4c28b0486c66d126
shjang7 <lucy.sh.jang@gmail.com>
2020-02-09 07:34:20 +0900

    Update gigasecond for float case

--- ruby_exercism/gigasecond/gigasecond.rb
+++ ruby_exercism/gigasecond/gigasecond.rb
@@ -1,5 +1,5 @@
 class Gigasecond
-  GIGA = 1_000_000_000
+  GIGA = 1_000_000_000.0
   def self.from(moment)
     moment + GIGA
   end
@@ -8,7 +8,7 @@ end
 if defined? Minitest
   describe 'Custom Test' do
     it 'must work out fractional calculations with no surprise' do
-      value(Gigasecond.from(0).to_f/60/60).must_equal 1e9/60/60
+      value(Gigasecond.from(0)/60/60).must_equal 1e9/60/60
     end
   end
 end
