d7efa01070205896d53ecdb9c9980712e1c1ee7f
shjang7 <lucy.sh.jang@gmail.com>
2020-02-09 05:55:30 +0900

    Update gigasecond for readability 1_000_xxx

--- ruby_exercism/gigasecond/gigasecond.rb
+++ ruby_exercism/gigasecond/gigasecond.rb
@@ -1,6 +1,14 @@
 class Gigasecond
-  GIGA = 10**9
+  GIGA = 1_000_000_000
   def self.from(moment)
     moment + GIGA
+  end
+end
+
+if defined? Minitest
+  describe 'Custom Test' do
+    it 'must work out fractional calculations with no surprise' do
+      value(Gigasecond.from(0).to_f/60/60).must_equal 1e9/60/60
+    end
   end
 end
