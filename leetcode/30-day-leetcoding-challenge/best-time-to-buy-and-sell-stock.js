a048979f9bbf4ae98f16a986a9a673c49f08def8
shjang7 <suhy.jang.shj@gmail.com>
2020-04-05 18:04:49 +0900

    Add best time to buy and sell stock

---
+++ leetcode/30-day-leetcoding-challenge/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function(prices) {
+    let profit = 0;
+    for(let i = 1; i < prices.length; i++) {
+        if (prices[i-1] < prices[i]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+    return profit;
+};
